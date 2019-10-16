856c7618d537387be78704b2695d283693b65867
Dietrich Epp <depp@zdome.net>
2019-10-16 04:52:15 -0400

    Add command-line options to error_gen tool

--- c/compiler/error_gen.c
+++ c/compiler/error_gen.c
@@ -1,3 +1,5 @@
+#include "c/compiler/argparse.h"
+
 #include <errno.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -5,8 +7,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+static const char USAGE[] =
+    "Error message compiler.\n"
+    "\n"
+    "Usage: error_gen [option...] <input>\n"
+    "\n"
+    "Options:\n"
+    "  -dump               Dump parsed messages to stdout.\n"
+    "  -help               Print command help.\n"
+    "  -include=<file.h>   Include file to include from generated sources.\n"
+    "  -msg-func=<name>    Use <name> as message lookup function name.\n"
+    "  -msg-out=<file.c>   Write message lookup function to <file.h>.\n"
+    "  -name-func=<name>   Use <name> as name lookup function name.\n"
+    "  -name-out=<file.c>  Write name lookup function to <file.h>.\n";
+
+// Print an error message for bad program usage and exit the program.
+static void die_usage(const char *msg) __attribute__((noreturn));
+static void die_usage(const char *msg) {
+    fprintf(stderr, "Error: %s\n", msg);
+    exit(2);
+}
+
+// Print a formatted error for bad program usage and exit the program.
+static void die_usagef(const char *msg, ...)
+    __attribute__((format(printf, 1, 2), noreturn));
+static void die_usagef(const char *msg, ...) {
+    fputs("Error: ", stderr);
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    fputc('\n', stderr);
+    exit(2);
+}
+
 static const char ERR_INPUT[] = "could not read input";
-static const char ERR_NOMEM[] = "out of memory";
 
 // Print an error message and exit the program. If ecode is not zero, append the
 // error code to the message.
@@ -20,11 +55,24 @@ static void die(int ecode, const char *msg) {
     exit(1);
 }
 
+static void die_nomem(void) __attribute__((noreturn));
+static void die_nomem(void) {
+    fputs("Error: out of memory\n", stderr);
+    exit(1);
+}
+
 // Print an error message for the given input line number and exit the program.
-static void dieline(int lineno, const char *msg) __attribute__((noreturn));
-static void dieline(int lineno, const char *msg) {
+static void die_input(int lineno, const char *msg) __attribute__((noreturn));
+static void die_input(int lineno, const char *msg) {
     fprintf(stderr, "Error: line %d: %s\n", lineno, msg);
     exit(1);
+}
+
+// Print an error message for an error returned by a function for writing
+// output.
+static void die_output(void) __attribute__((noreturn));
+static void die_output(void) {
+    die(errno, "could not write output");
 }
 
 // Return true if the range has the given prefix.
@@ -68,7 +116,7 @@ static char *copystr(const char *start, const char *end) {
     size_t size = end - start;
     char *text = malloc(size + 1);
     if (text == NULL) {
-        die(errno, ERR_NOMEM);
+        die_nomem();
     }
     memcpy(text, start, size);
     text[size] = '\0';
@@ -98,7 +146,7 @@ static struct errs read_input(const char *filename) {
             if (ferror(fp)) {
                 die(errno, ERR_INPUT);
             }
-            dieline(lineno, "could not find start of error codes");
+            die_input(lineno, "could not find start of error codes");
         }
         lineno++;
         if (memcmp(line, "enum {", 6) == 0) {
@@ -112,7 +160,7 @@ static struct errs read_input(const char *filename) {
             if (ferror(fp)) {
                 die(errno, ERR_INPUT);
             }
-            dieline(lineno, "could not find end of error codes");
+            die_input(lineno, "could not find end of error codes");
         }
         lineno++;
         const char *start = line;
@@ -125,12 +173,13 @@ static struct errs read_input(const char *filename) {
             start = trim_start(start + 2, end);
             if (start != end) {
                 if (text != NULL) {
-                    dieline(lineno, "multiple description for same error");
+                    die_input(lineno, "multiple description for same error");
                 }
                 for (const char *ptr = start; ptr != end; ptr++) {
                     unsigned char c = *ptr;
                     if (c < 32 || c > 126) {
-                        dieline(lineno, "description contains non-ASCII text");
+                        die_input(lineno,
+                                  "description contains non-ASCII text");
                     }
                 }
                 text = copystr(start, end);
@@ -138,7 +187,7 @@ static struct errs read_input(const char *filename) {
         } else if (has_prefix(start, end, "ERR_")) {
             // Error name.
             if (text == NULL) {
-                dieline(lineno, "error code has no description");
+                die_input(lineno, "error code has no description");
             }
             start += 4;
             end = start;
@@ -146,7 +195,7 @@ static struct errs read_input(const char *filename) {
                 end++;
             }
             if (start == end) {
-                dieline(lineno, "invalid error code name");
+                die_input(lineno, "invalid error code name");
             }
             char *name = copystr(start, end);
             if (errs.count >= errs.alloc) {
@@ -154,12 +203,12 @@ static struct errs read_input(const char *filename) {
                 errs.names =
                     realloc(errs.names, errs.alloc * sizeof(errs.names));
                 if (errs.names == NULL) {
-                    die(errno, ERR_NOMEM);
+                    die_nomem();
                 }
                 errs.texts =
                     realloc(errs.texts, errs.alloc * sizeof(errs.texts));
                 if (errs.texts == NULL) {
-                    die(errno, ERR_NOMEM);
+                    die_nomem();
                 }
             }
             errs.names[errs.count] = name;
@@ -169,28 +218,26 @@ static struct errs read_input(const char *filename) {
         } else if (has_prefix(start, end, "};")) {
             // End of errors.
             if (text != NULL) {
-                dieline(lineno, "expected error code name");
+                die_input(lineno, "expected error code name");
             }
             if (errs.count == 0) {
-                dieline(lineno, "no error codes found");
+                die_input(lineno, "no error codes found");
             }
             break;
         } else {
-            dieline(lineno, "could not parse error");
+            die_input(lineno, "could not parse error");
         }
     }
     fclose(fp);
+    if (errs.count == 0) {
+        die(errno, "no messages found in input");
+    }
     return errs;
-}
-
-static void diewrite(void) __attribute__((noreturn));
-static void diewrite(void) {
-    die(errno, "could not write output");
 }
 
 static void cputs(FILE *fp, const char *s) {
     if (fputs(s, fp) < 0) {
-        diewrite();
+        die_output();
     }
 }
 
@@ -200,7 +247,7 @@ static void cprintf(FILE *fp, const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     if (vfprintf(fp, fmt, ap) < 0) {
-        diewrite();
+        die_output();
     }
     va_end(ap);
 }
@@ -208,21 +255,88 @@ static void cprintf(FILE *fp, const char *fmt, ...) {
 static void cwrite(FILE *fp, const char *p, size_t size) {
     size_t r = fwrite(p, 1, size, fp);
     if (r != size) {
-        diewrite();
+        die_output();
     }
+}
+
+static const char HEXDIGIT[16] = "0123456789abcdef";
+
+// Write a character in a C string to the given buffer, and return the advanced
+// pointer.
+static char *write_char(char *buf, unsigned char c) {
+    char *out = buf;
+    if (32 <= c && c <= 126) {
+        if (c == '\\' || c == '"') {
+            *out++ = '\\';
+        }
+        *out++ = c;
+        return out;
+    }
+    *out++ = '\\';
+    unsigned char e;
+    switch (c) {
+    case '\n':
+        e = 'n';
+        break;
+    case '\r':
+        e = 'r';
+        break;
+    case '\t':
+        e = 't';
+        break;
+    default:
+        out[0] = 'x';
+        out[1] = HEXDIGIT[c >> 4];
+        out[2] = HEXDIGIT[c & 15];
+        return out + 3;
+    }
+    *out++ = e;
+    return out;
+}
+
+// Write a C string to the given buffer, and return the advanced pointer.
+static char *write_str(char *buf, const char *restrict str) {
+    char *out = buf;
+    for (const char *ptr = str; *ptr != '\0'; ptr++) {
+        out = write_char(out, *ptr);
+    }
+    return out;
+}
+
+// Dump the parsed errors to standard output.
+static void dump_errs(const struct errs *restrict errs) {
+    size_t maxmsg = 0;
+    for (size_t i = 0; i < errs->count; i++) {
+        size_t len = strlen(errs->texts[i]);
+        if (len > maxmsg) {
+            maxmsg = len;
+        }
+    }
+    char *buf = malloc(maxmsg * 4 + 1);
+    if (buf == NULL) {
+        die_nomem();
+    }
+    for (size_t i = 0; i < errs->count; i++) {
+        cprintf(stdout, "%zu %s \"", i, errs->names[i]);
+        char *out = write_str(buf, errs->texts[i]);
+        *out = '\0';
+        cwrite(stdout, buf, out - buf);
+        cputs(stdout, "\"\n");
+    }
+    free(buf);
 }
 
 // Write a string array function to the given output file. If the output file is
 // NULL, write to stdout.
 static void write_array(const char *filename, char **arr, size_t count,
-                        const char *funcname) {
+                        const char *funcname, const char *include) {
     if (count == 0) {
         die(errno, "zero array size");
     }
     // Calculate string offsets.
     size_t *offsets = malloc(sizeof(*offsets) * count);
     if (offsets == NULL) {
-        die(errno, ERR_NOMEM);
+        die_nomem();
     }
     size_t offset = 0;
     size_t maxsize = 0;
@@ -236,7 +350,7 @@ static void write_array(const char *filename, char **arr, size_t count,
     }
     // Write output.
     FILE *fp;
-    if (filename == NULL) {
+    if (strcmp(filename, "-") == 0) {
         fp = stdout;
     } else {
         fp = fopen(filename, "w");
@@ -245,25 +359,21 @@ static void write_array(const char *filename, char **arr, size_t count,
         }
     }
     cputs(fp, "/* This file is automatically generated. */\n");
-    cputs(fp, "#include \"c/compiler/error.h\"\n");
+    if (include != NULL) {
+        cprintf(fp, "#include \"%s\"\n", include);
+    }
     cputs(fp, "#include <stdint.h>\n");
     cprintf(fp, "#define ERR_COUNT %zu\n", count);
     cputs(fp, "static char ERR_TEXT[] =\n");
-    char *buf = malloc(9 + maxsize * 2);
+    char *buf = malloc(9 + maxsize * 4);
     if (buf == NULL) {
-        die(errno, ERR_NOMEM);
+        die_nomem();
     }
     for (size_t i = 0; i < count; i++) {
         char *out = buf;
         memcpy(out, "    \"", 5);
         out += 5;
-        for (const char *ptr = arr[i]; *ptr; ptr++) {
-            unsigned char c = *ptr;
-            if (c == '\\' || c == '"') {
-                *out++ = '\\';
-            }
-            *out++ = c;
-        }
+        out = write_str(out, arr[i]);
         if (i + 1 == count) {
             memcpy(out, "\";\n", 3);
             out += 3;
@@ -294,31 +404,119 @@ static void write_array(const char *filename, char **arr, size_t count,
     cputs(fp, "    }\n");
     cputs(fp, "    return ERR_TEXT + ERR_OFFSET[err];\n");
     cputs(fp, "}\n");
-    if (filename != NULL) {
+    if (fp != stdout) {
         if (fclose(fp) < 0) {
-            diewrite();
+            die_output();
+        }
+    }
+}
+
+struct args {
+    bool dump;
+    const char *input;
+    const char *include;
+    const char *msg_func;
+    const char *msg_out;
+    const char *name_func;
+    const char *name_out;
+};
+
+enum {
+    OPT_DUMP,
+    OPT_HELP,
+    OPT_INCLUDE,
+    OPT_MSG_FUNC,
+    OPT_MSG_OUT,
+    OPT_NAME_FUNC,
+    OPT_NAME_OUT,
+};
+
+static const struct ufxr_argdef ARG_DEFS[] = {
+    {OPT_DUMP, "dump", ARG_BARE},
+    {OPT_HELP, "help", ARG_BARE},
+    {OPT_INCLUDE, "include", ARG_STRING},
+    {OPT_MSG_FUNC, "msg-func", ARG_STRING},
+    {OPT_MSG_OUT, "msg-out", ARG_STRING},
+    {OPT_NAME_FUNC, "name-func", ARG_STRING},
+    {OPT_NAME_OUT, "name-out", ARG_STRING},
+    {},
+};
+
+static void parse_args(struct args *args, int argc, char **argv) {
+    struct ufxr_argparser ap;
+    ufxr_argparser_init(&ap, argc, argv);
+    while (true) {
+        int r = ufxr_argparser_next(&ap, ARG_DEFS);
+        if (r < 0) {
+            char *msg;
+            switch (r) {
+            case ARG_END:
+                if (args->input == NULL) {
+                    die_usage("input file not specified");
+                }
+                if (args->msg_out != NULL && args->msg_func == NULL) {
+                    die_usage("-msg-out requires -msg-func to be specified");
+                }
+                if (args->name_out != NULL && args->name_func == NULL) {
+                    die_usage("-name-out requires -name-func to be specified");
+                }
+                return;
+            case ARG_POSITIONAL:
+                if (args->input != NULL) {
+                    die_usagef("unexpected argument '%s'", ap.val);
+                }
+                args->input = ap.val;
+                break;
+            default:
+                msg = ufxr_argparser_err(&ap, r);
+                if (msg == NULL) {
+                    die_nomem();
+                }
+                die_usage(msg);
+            }
+        } else {
+            switch (r) {
+            case OPT_DUMP:
+                args->dump = true;
+                break;
+            case OPT_HELP:
+                cputs(stdout, USAGE);
+                exit(0);
+                break;
+            case OPT_INCLUDE:
+                args->include = ap.val;
+                break;
+            case OPT_MSG_FUNC:
+                args->msg_func = ap.val;
+                break;
+            case OPT_MSG_OUT:
+                args->msg_out = ap.val;
+                break;
+            case OPT_NAME_FUNC:
+                args->name_func = ap.val;
+                break;
+            case OPT_NAME_OUT:
+                args->name_out = ap.val;
+                break;
+            }
         }
     }
 }
 
 int main(int argc, char **argv) {
-    const char *input, *outname, *outtext;
-    switch (argc) {
-    case 2:
-        input = argv[1];
-        outname = NULL;
-        outtext = NULL;
-        break;
-    case 4:
-        input = argv[1];
-        outname = argv[2];
-        outtext = argv[3];
-        break;
-    default:
-        die(0, "invalid usage");
+    struct args args = {};
+    parse_args(&args, argc - 1, argv + 1);
+    struct errs errs = read_input(args.input);
+    if (args.dump) {
+        dump_errs(&errs);
     }
-    struct errs errs = read_input(input);
-    write_array(outname, errs.names, errs.count, "ufxr_errname");
-    write_array(outtext, errs.texts, errs.count, "ufxr_errtext");
+    if (args.name_out != NULL) {
+        write_array(args.name_out, errs.names, errs.count, args.name_func,
+                    args.include);
+    }
+    if (args.msg_out != NULL) {
+        write_array(args.msg_out, errs.texts, errs.count, args.msg_func,
+                    args.include);
+    }
     return 0;
 }
