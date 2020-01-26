89fbf86d0a7c2a18b4091710e616097b7d5f4f8b
Dietrich Epp <depp@zdome.net>
2020-01-26 16:17:12 -0500

    Improve format for printing tokens

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,30 @@
 mod sourcepos;
 mod token;
 
-use token::{Tokenizer, Type};
+use sourcepos::Pos;
+use std::fmt;
+use std::str::from_utf8;
+use token::{Token, Tokenizer, Type};
 
-const text: &'static str = "(abc def)";
+const TEXT: &'static str = "(abc def)";
+
+// Print a token to stdout for debugging.
+fn print_token(tok: &Token) {
+    let Token { ty, pos, text } = tok;
+    let Pos(off) = pos;
+    let utf8 = from_utf8(text);
+    let dtext: &dyn fmt::Debug = match utf8 {
+        Ok(ref s) => s,
+        Err(_) => &text,
+    };
+    println!("{:4} {:?} {:?}", off, ty, dtext);
+}
 
 fn main() {
-    let mut toks = Tokenizer::new(text.as_bytes());
+    let mut toks = Tokenizer::new(TEXT.as_bytes());
     loop {
         let tok = toks.next();
-        println!("tok: {:?}", tok);
+        print_token(&tok);
         if tok.ty == Type::End {
             break;
         }
