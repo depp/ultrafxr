6df8686e07cc392f761dd0b5f940e0ed7ad5d1aa
Dietrich Epp <depp@zdome.net>
2020-03-04 23:12:50 -0500

    Divide evaluate module into submodules

---
+++ rust/src/evaluate/builtins.rs
@@ -0,0 +1,178 @@
+use super::environment::*;
+use crate::graph::op;
+use crate::sexpr::SExpr;
+use crate::sourcepos::{HasPos, Span};
+use crate::units::Units;
+use std::collections::hash_map::{HashMap, RandomState};
+use std::convert::TryFrom;
+
+pub fn operators() -> HashMap<&'static str, Operator, RandomState> {
+    let mut map = HashMap::new();
+    fn add(
+        map: &mut HashMap<&'static str, Operator, RandomState>,
+        name: &'static str,
+        value: Operator,
+    ) {
+        if map.insert(name, value).is_some() {
+            panic!("duplicate operator name: {:?}", name);
+        }
+    }
+    const UNIMPL_MACROS: &'static [&'static str] = &[
+        //
+        "envelope",
+    ];
+    for &name in UNIMPL_MACROS.iter() {
+        add(&mut map, name, Operator::Macro(None));
+    }
+    macro_rules! macros {
+            ($($f:ident);*;) => {
+                $(add(&mut map, stringify!($f), Operator::Macro(Some($f)));)*
+            };
+        }
+    macro_rules! function {
+        ($name:literal, !) => {
+            add(&mut map, $name, Operator::Function(None))
+        };
+        ($name:literal, $func:ident) => {
+            add(&mut map, $name, Operator::Function(Some($func)))
+        };
+    }
+    macro_rules! functions {
+            ($($name:literal => $val:tt),*,) => {
+                $(function!($name, $val));*
+            };
+        }
+    macros!(
+        define;
+    );
+    functions!(
+        //
+        "*" => !,
+        "note" => note,
+        "oscillator" => !,
+        "sawtooth" => !,
+        "sine" => !,
+        "noise" => !,
+        "highPass" => !,
+        "lowPass2" => !,
+        "highPass2" => !,
+        "bandPass2" => !,
+        "lowPass4" => !,
+        "saturate" => !,
+        "rectify" => !,
+        "multiply" => !,
+        "frequency" => !,
+        "mix" => !,
+        "phase-mod" => !,
+        "overtone" => !,
+    );
+    map
+}
+
+// =============================================================================================
+// Macros
+// =============================================================================================
+
+/// Wrap a macro argument with information about its name and source location.
+fn macro_arg<'a>(name: &'static str, expr: &'a SExpr) -> EvalResult<&'a SExpr> {
+    let label = ValueLabel {
+        pos: expr.source_pos(),
+        name: Some(name),
+        index: 0,
+    };
+    EvalResult(label, Ok(expr))
+}
+
+fn define<'a>(env: &mut Env<'a>, _pos: Span, args: &'a [SExpr]) -> OpResult {
+    let (name, value) = match args {
+        [name, value] => (macro_arg("name", name), macro_arg("value", value)),
+        _ => {
+            return Err(OpError::BadNArgs {
+                got: args.len(),
+                min: 2,
+                max: Some(2),
+            });
+        }
+    };
+    let mut name = name.and_then(get_symbol);
+    match name.value() {
+        Some(nameval) => {
+            if env.variables.contains_key(nameval) {
+                name.1 = error!(
+                    env,
+                    name.source_pos(),
+                    "a variable named {:?} is already defined",
+                    nameval
+                );
+            }
+        }
+        _ => (),
+    };
+    let name = name.unwrap(env);
+    let value = value.evaluate(env).into_nonvoid().unwrap(env);
+    let name = name?;
+    env.variables.insert(name, value);
+    value?;
+    Ok(Value::void())
+}
+
+// envelope
+
+// =============================================================================================
+// Parameters
+// =============================================================================================
+
+fn note(env: &mut Env, pos: Span, args: &[EvalResult<Value>]) -> OpResult {
+    let offset = match args {
+        [offset] => offset.clone(),
+        _ => {
+            return Err(OpError::BadNArgs {
+                got: args.len(),
+                min: 1,
+                max: Some(1),
+            });
+        }
+    };
+    let offset = offset
+        .into_int()
+        .and_then(|i| i32::try_from(i).map_err(|_| unimplemented!()))
+        .unwrap(env);
+    let offset = offset?;
+    Ok(env.new_node(pos, Units::hertz(1), op::Note { offset }))
+}
+
+// =============================================================================================
+// Oscillators and generators
+// =============================================================================================
+
+// oscillator
+// sawtooth
+// sine
+// noise
+
+// =============================================================================================
+// Filters
+// =============================================================================================
+
+// high_pass
+// low_pass_2
+// high_pass_2
+// band_pass_2
+// low_pass_4
+
+// =============================================================================================
+// Distortion
+// =============================================================================================
+
+// saturate
+// rectify
+
+// =============================================================================================
+// Utilities
+// =============================================================================================
+
+// multiply
+// frequency
+// mix
+// phase_mod
+// overtone
---
+++ rust/src/evaluate/environment.rs
@@ -0,0 +1,426 @@
+use crate::error::ErrorHandler;
+use crate::graph::{Graph, Node, SignalRef};
+use crate::sexpr::{Content, SExpr, Type as EType};
+use crate::sourcepos::{HasPos, Span};
+use crate::units::Units;
+use std::collections::hash_map::{HashMap, RandomState};
+use std::convert::From;
+use std::fmt::{Display, Formatter, Result as FResult};
+
+/// Error for evaluation failure. The error diagnostics are reported elsewhere.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Failed;
+
+/// Log an error, returning void.
+macro_rules! log_error {
+    ($env:expr, $loc:expr, $msg:literal) => {
+        $env.error($loc, $msg);
+    };
+    ($env:expr, $loc:expr, $($tts:expr),*) => {
+        $env.error($loc, format!($($tts),*).as_ref());
+    };
+}
+
+/// Log an error and return an evaluation failure.
+macro_rules! error {
+    ($env:expr, $loc:expr, $($tts:expr),*) => {{
+        log_error!($env, $loc, $($tts),*);
+        Err(From::from($crate::evaluate::environment::Failed))
+    }};
+}
+
+/// Error for interpreting a value.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ValueError {
+    Failed,
+    BadType { got: Type, expect: Type },
+    BadEType { got: EType, expect: EType },
+}
+
+impl Display for ValueError {
+    fn fmt(&self, f: &mut Formatter) -> FResult {
+        use ValueError::*;
+        match self {
+            Failed => write!(f, "evaluation failed"),
+            BadType { got, expect } => write!(f, "type is {}, expected {}", got, expect),
+            BadEType { got, expect } => write!(f, "type is {}, expected {}", got, expect),
+        }
+    }
+}
+
+impl From<Failed> for ValueError {
+    fn from(_: Failed) -> Self {
+        ValueError::Failed
+    }
+}
+
+/// Error for various operations during evaluation.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum OpError {
+    Failed,
+    BadNArgs {
+        got: usize,
+        min: usize,
+        max: Option<usize>,
+    },
+}
+
+impl Display for OpError {
+    fn fmt(&self, f: &mut Formatter) -> FResult {
+        use OpError::*;
+        match self {
+            Failed => write!(f, "evaluation failed"),
+            BadNArgs { got, min, max } => match max {
+                Some(max) => {
+                    if min < max {
+                        write!(f, "got {} args, expected {}-{}", got, min, max)
+                    } else {
+                        write!(f, "got {} args, expected {}", got, min)
+                    }
+                }
+                None => write!(f, "got {} args, expected at least {}", got, min),
+            },
+        }
+    }
+}
+
+impl From<Failed> for OpError {
+    fn from(_: Failed) -> Self {
+        OpError::Failed
+    }
+}
+
+/// The data within a value, without units.
+#[derive(Debug, Clone, Copy)]
+enum Data {
+    Int(i64),
+    Float(f64),
+    Signal(SignalRef),
+    Void,
+}
+
+impl Data {
+    fn data_type(&self) -> DataType {
+        match self {
+            Data::Int(_) => DataType::Int,
+            Data::Float(_) => DataType::Float,
+            Data::Signal(_) => DataType::Signal,
+            Data::Void => DataType::Void,
+        }
+    }
+}
+
+/// A value from evaluating an expression successfully.
+#[derive(Debug, Clone, Copy)]
+pub struct Value(Data, Units);
+
+impl Value {
+    pub fn void() -> Self {
+        Value(Data::Void, Units::scalar())
+    }
+
+    fn get_type(&self) -> Type {
+        Type(self.0.data_type(), Some(self.1))
+    }
+
+    fn bad_type(&self, expect: Type) -> ValueError {
+        ValueError::BadType {
+            got: self.get_type(),
+            expect,
+        }
+    }
+
+    fn into_void(self) -> Result<(), ValueError> {
+        match self {
+            Value(Data::Void, _) => Ok(()),
+            _ => Err(self.bad_type(Type::void())),
+        }
+    }
+
+    fn into_nonvoid(self) -> Result<Value, ValueError> {
+        match self {
+            Value(Data::Void, _) => Err(self.bad_type(Type(DataType::NonVoid, None))),
+            val => Ok(val),
+        }
+    }
+
+    fn into_int(self) -> Result<i64, ValueError> {
+        match self {
+            Value(Data::Int(num), units) if units.is_scalar() => Ok(num),
+            val => Err(val.bad_type(Type(DataType::Int, Some(Units::scalar())))),
+        }
+    }
+
+    fn into_signal(self, units: Units) -> Result<SignalRef, ValueError> {
+        match self {
+            Value(Data::Signal(sig), vunits) if vunits == units => Ok(sig),
+            val => Err(val.bad_type(Type(DataType::Signal, Some(units)))),
+        }
+    }
+}
+
+/// Result of evaluating function or macro body.
+pub type OpResult = Result<Value, OpError>;
+
+/// A type of data within a value, or void.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DataType {
+    Void,
+    Signal,
+    Int,
+    Float,
+    NonVoid,
+}
+
+/// A type of a value with its units.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Type(pub DataType, pub Option<Units>);
+
+impl Type {
+    fn void() -> Self {
+        Type(DataType::Void, None)
+    }
+}
+
+impl Display for Type {
+    fn fmt(&self, f: &mut Formatter) -> FResult {
+        f.write_str(match self.0 {
+            DataType::Void => "void",
+            DataType::Signal => "signal",
+            DataType::Int => "int",
+            DataType::Float => "float",
+            DataType::NonVoid => "non-void",
+        })?;
+        match self.1 {
+            Some(u) => write!(f, "({})", u),
+            None => Ok(()),
+        }
+    }
+}
+
+/// Identifying information about a value so we can generate better error
+/// messages for it.
+#[derive(Debug, Clone, Copy)]
+pub struct ValueLabel {
+    pub pos: Span,
+    pub name: Option<&'static str>,
+    pub index: usize,
+}
+
+/// Result of evaluating a subexpression.
+#[derive(Debug, Clone)]
+pub struct EvalResult<T>(pub ValueLabel, pub Result<T, ValueError>);
+
+impl<T> EvalResult<T> {
+    pub fn and_then<U, F>(self, op: F) -> EvalResult<U>
+    where
+        F: FnOnce(T) -> Result<U, ValueError>,
+    {
+        match self {
+            EvalResult(label, v) => EvalResult(label, v.and_then(op)),
+        }
+    }
+
+    pub fn unwrap(self, env: &mut Env) -> Result<T, Failed> {
+        let EvalResult(label, value) = self;
+        match value {
+            Ok(value) => Ok(value),
+            Err(ValueError::Failed) => Err(Failed),
+            Err(e) => {
+                let msg = match label.name {
+                    Some(name) => format!("invalid value for {}: {}", name, e),
+                    None => format!("invalid value: {}", e),
+                };
+                env.error(label.pos, msg.as_str());
+                Err(Failed)
+            }
+        }
+    }
+}
+
+impl<T> EvalResult<T>
+where
+    T: Copy,
+{
+    pub fn value(&self) -> Option<T> {
+        match self {
+            EvalResult(_, Ok(v)) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl<T> HasPos for EvalResult<T> {
+    fn source_pos(&self) -> Span {
+        match self {
+            EvalResult(label, _) => label.pos,
+        }
+    }
+}
+
+impl EvalResult<Value> {
+    pub fn into_void(self) -> EvalResult<()> {
+        self.and_then(Value::into_void)
+    }
+
+    pub fn into_nonvoid(self) -> EvalResult<Value> {
+        self.and_then(Value::into_nonvoid)
+    }
+
+    pub fn into_int(self) -> EvalResult<i64> {
+        self.and_then(Value::into_int)
+    }
+
+    pub fn into_signal(self, units: Units) -> EvalResult<SignalRef> {
+        self.and_then(|v| v.into_signal(units))
+    }
+}
+
+impl<'a> EvalResult<&'a SExpr> {
+    pub fn evaluate(self, env: &mut Env<'a>) -> EvalResult<Value> {
+        let EvalResult(label, value) = self;
+        let value = match value {
+            Ok(form) => match env.evaluate_impl(form) {
+                Ok(v) => Ok(v),
+                Err(Failed) => Err(ValueError::Failed),
+            },
+            Err(e) => Err(e),
+        };
+        EvalResult(label, value)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Operator {
+    Function(Option<fn(&mut Env, Span, &[EvalResult<Value>]) -> OpResult>),
+    Macro(Option<for<'a> fn(&mut Env<'a>, Span, &'a [SExpr]) -> OpResult>),
+}
+
+/// An environment for evaluating s-expressions.
+pub struct Env<'a> {
+    has_error: bool,
+    err_handler: &'a mut dyn ErrorHandler,
+    pub variables: HashMap<&'a str, Result<Value, Failed>, RandomState>,
+    operators: HashMap<&'a str, Operator, RandomState>,
+    graph: Graph,
+    #[allow(dead_code)]
+    tail_length: Option<f64>,
+}
+
+impl<'a> Env<'a> {
+    /// Create a new environment with the given functions defined.
+    pub fn new(
+        err_handler: &'a mut dyn ErrorHandler,
+        operators: HashMap<&'a str, Operator, RandomState>,
+    ) -> Self {
+        Env {
+            has_error: false,
+            err_handler,
+            variables: HashMap::new(),
+            operators,
+            graph: Graph::new(),
+            tail_length: None,
+        }
+    }
+
+    /// Evaluate an s-expression.
+    pub fn evaluate(&mut self, expr: &'a SExpr) -> EvalResult<Value> {
+        let label = ValueLabel {
+            pos: expr.source_pos(),
+            name: None,
+            index: 0,
+        };
+        let value = match self.evaluate_impl(expr) {
+            Ok(value) => Ok(value),
+            Err(Failed) => Err(ValueError::Failed),
+        };
+        EvalResult(label, value)
+    }
+
+    fn evaluate_impl(&mut self, expr: &'a SExpr) -> Result<Value, Failed> {
+        let pos = expr.source_pos();
+        match &expr.content {
+            &Content::Symbol(ref name) => match self.variables.get(name.as_ref()) {
+                Some(&value) => value,
+                None => error!(self, pos, "undefined symbol: {:?}", name),
+            },
+            &Content::Integer(units, num) => Ok(Value(Data::Int(num), units)),
+            &Content::Float(units, num) => Ok(Value(Data::Float(num), units)),
+            &Content::List(ref items) => {
+                let (op, args) = match items.split_first() {
+                    Some(x) => x,
+                    None => return error!(self, pos, "cannot evaluate empty list"),
+                };
+                let name: &str = match &op.content {
+                    &Content::Symbol(ref name) => name.as_ref(),
+                    _ => return error!(self, pos, "function or macro name must be a symbol"),
+                };
+                let oppos = op.source_pos();
+                let op = match self.operators.get(name) {
+                    Some(x) => *x,
+                    None => return error!(self, oppos, "undefined function or macro: {:?}", name),
+                };
+                let r = match op {
+                    Operator::Function(f) => {
+                        if f.is_none() {
+                            log_error!(self, oppos, "unimplemented function: {}", name);
+                        }
+                        let mut values: Vec<EvalResult<Value>> = Vec::with_capacity(args.len());
+                        for arg in args.iter() {
+                            values.push(self.evaluate(arg));
+                        }
+                        match f {
+                            Some(f) => f(self, pos, &values),
+                            None => Err(OpError::Failed),
+                        }
+                    }
+                    Operator::Macro(f) => match f {
+                        Some(f) => f(self, pos, args),
+                        None => error!(self, oppos, "unimplemented macro: {}", name),
+                    },
+                };
+                match r {
+                    Ok(val) => Ok(val),
+                    Err(OpError::Failed) => Err(Failed),
+                    Err(e) => error!(self, pos, "invalid call to {:?}: {}", name, e),
+                }
+            }
+        }
+    }
+
+    /// Log an error message.
+    pub fn error(&mut self, pos: Span, msg: &str) {
+        self.has_error = true;
+        self.err_handler.handle(pos, msg);
+    }
+
+    /// Add a new audio processing node to the graph.
+    pub fn new_node(&mut self, pos: Span, units: Units, node: impl Node) -> Value {
+        self.new_node_box(pos, units, Box::new(node))
+    }
+
+    fn new_node_box(&mut self, _pos: Span, units: Units, node: Box<dyn Node>) -> Value {
+        let sig = self.graph.add(node);
+        Value(Data::Signal(sig), units)
+    }
+
+    /// Discard the environment and return the created graph.
+    pub fn into_graph(self) -> Option<Graph> {
+        if self.has_error {
+            None
+        } else {
+            Some(self.graph)
+        }
+    }
+}
+
+/// Get the name of a symbol.
+pub fn get_symbol(expr: &SExpr) -> Result<&str, ValueError> {
+    match &expr.content {
+        &Content::Symbol(ref name) => Ok(name),
+        _ => Err(ValueError::BadEType {
+            got: expr.get_type(),
+            expect: EType::Symbol,
+        }),
+    }
+}
