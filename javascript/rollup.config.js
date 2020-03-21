fb0b3d4140f828fa6ce0a22cfc278fad1a697281
Dietrich Epp <depp@zdome.net>
2020-03-21 10:34:16 -0400

    Add banner to bundled JS

--- javascript/rollup.config.js
+++ javascript/rollup.config.js
@@ -1,9 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+const banner = `/**
+ * UltraFXR v${pkg.version}
+ * Copyright (c) 2019-2020 Dietrich Epp
+ * https://www.ultrafxr.us/
+ * @license MIT
+ */`;
 export default {
   input: 'src/index.ts',
   output: {
     dir: 'build',
     format: 'es',
+    banner,
   },
   plugins: [
     typescript({
