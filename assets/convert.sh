b3bbddc624863971d54bec09824f1c59a4b505b6
Dietrich Epp <depp@zdome.net>
2020-03-22 17:24:33 -0400

    Rename 'icon2' to 'tile'
    This is to distinguish between the home screen tiles (with a solid
    background) and the favicons (with a transparent background).

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 for size in 192 512 ; do
-  output="icon${size}.png"
+  output="tile${size}.png"
   inkscape --without-gui \
 	   --export-png="${output}" \
 	   --export-width="${size}" \
 	   --export-height="${size}" \
-	   icon2.svg
+	   tile.svg
   optipng -strip all "${output}"
 done
