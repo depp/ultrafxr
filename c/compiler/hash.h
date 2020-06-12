cfc4edd0a33d84882143ce9e6c261a3ad300998c
Dietrich Epp <depp@zdome.net>
2020-06-12 05:03:25 -0400

    Remove C version of compiler
    Use the Rust version instead, which is actually complete.

--- c/compiler/hash.h
+++
@@ -1,8 +0,0 @@
-// hash.h - Simple hash function.
-#pragma once
-
-#include <stddef.h>
-#include <stdint.h>
-
-// Compute the hash of a bytestring.
-uint32_t ufxr_hash(const void *data, size_t size) __attribute__((pure));
