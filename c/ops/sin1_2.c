47959afdbb166b0c9b6a6f9727c6524f4f3ae930
Dietrich Epp <depp@zdome.net>
2020-06-21 16:23:14 -0400

    Improve scalar performance of sin1_2

--- c/ops/sin1_2.c
+++ c/ops/sin1_2.c
@@ -27,13 +27,7 @@ void ufxr_sin1_2(int n, float *restrict outs, const float *restrict xs) {
     assert((n % UFXR_QUANTUM) == 0);
     for (int i = 0; i < n; i++) {
         float x = xs[i];
-        x -= (float)(int)x;
-        float t1 = 0.5f - x;
-        float t2 = -0.5f - x;
-        if (t1 < x)
-            x = t1;
-        if (t2 > x)
-            x = t2;
+        x -= rintf(x);
         outs[i] = x * (8.0f - 16.0f * fabsf(x));
     }
 }
