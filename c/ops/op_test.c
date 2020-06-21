5a869f42b05ca3f176ff59b60ced46444e4e58e2
Dietrich Epp <depp@zdome.net>
2020-06-20 22:04:52 -0400

    Use exponential notation for error

--- c/ops/op_test.c
+++ c/ops/op_test.c
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <stdlib.h>
 
+// Calculate exponential function error in cents.
 static float exp2_err(int n, const float *restrict ys,
                       const float *restrict xs) {
     float max_error = -1.0f;
@@ -31,6 +32,7 @@ static float tri(float x) {
     return x - 4.0f;
 }
 
+// Calculate triangle function error as maximum difference.
 static float tri_err(int n, const float *restrict ys,
                      const float *restrict xs) {
     float max_error = -1.0f;
@@ -56,17 +58,17 @@ struct func_info {
 #define F(f, g, e) \
     { #f, ufxr_##f, g, e }
 static const struct func_info kFuncs[] = {
-    F(exp2_2, exp2_err, 2.989197f), //
-    F(exp2_3, exp2_err, 0.129604f), //
-    F(exp2_4, exp2_err, 0.004721f), //
-    F(exp2_5, exp2_err, 0.000572f), //
-    F(exp2_6, exp2_err, 0.000286f), //
+    F(exp2_2, exp2_err, 2.9888e0),  //
+    F(exp2_3, exp2_err, 1.2960e-1), //
+    F(exp2_4, exp2_err, 4.7207e-3), //
+    F(exp2_5, exp2_err, 5.7220e-4), //
+    F(exp2_6, exp2_err, 2.8610e-4), //
     F(tri, tri_err, 2.0f),          //
 };
 #undef F
 
 // Extra margin for error, a ratio.
-static const float kErrorMargin = 1.005f;
+static const float kErrorMargin = 0.005f;
 
 int main(int argc, char **argv) {
     int size = 1 << 20;
@@ -89,11 +91,13 @@ int main(int argc, char **argv) {
         printf("Testing: %s\n", kFuncs[i].name);
         kFuncs[i].func(size, ys, xs);
         float error = kFuncs[i].errf(size, ys, xs);
-        printf("Error: %g\n", (double)error);
-        printf("Max error: %g\n", (double)kFuncs[i].error);
-        if (error > kFuncs[i].error * kErrorMargin) {
+        printf("Error: %.4e\n", (double)error);
+        printf("Max error: %.4e\n", (double)kFuncs[i].error);
+        if (error > kFuncs[i].error * (1.0f + kErrorMargin)) {
             puts("****FAIL****");
             success = false;
+        } else if (error < kFuncs[i].error * (1.0f - kErrorMargin)) {
+            puts("****IMPROVED****");
         }
         putc('\n', stdout);
         fflush(stdout);
